521cc7784f10a630e453712c912b766ac2671599
tayga2410 <moldazhanov.eldar@gmail.com>
2023-12-20 01:16:47 +0600

    тесты в порядке. перехожу на ПП тест

--- source/js/vendor/toggle-iframe.js
+++ source/js/vendor/toggle-iframe.js
@@ -1,17 +1,16 @@
 function toggleIframe() {
-  const wrapper = document.querySelector('.hero__vidget-wrapper');
-  const button = wrapper.querySelector('.hero__vidget-button');
-  const image = wrapper.querySelector('img');
-  const iframe = wrapper.querySelector('iframe');
+  const wrapper = document.querySelector(".hero__vidget-wrapper");
+  const button = wrapper.querySelector(".hero__vidget-button");
+  const image = wrapper.querySelector("img");
+  const iframe = wrapper.querySelector("iframe");
 
   if (button && image && iframe) {
-
-    button.addEventListener('click', function() {
+    button.addEventListener("click", function () {
       button.style.display = "none";
       image.style.display = "none";
       iframe.style.display = "block";
     });
-  } 
+  }
 }
 
 export default toggleIframe;
